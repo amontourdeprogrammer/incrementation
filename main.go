8ca8afc686572773e94fc94b6bb2a8fcfd47b91f
Votre nom ou pseudo <Votre@email.comEliPamp>
2017-04-05 15:37:36 +0200

    creation du main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import (
+  "log"
+  "net/http"
+  "fmt"
+  "html"
+
+)
+
+func main () {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+  })
+
+  http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hi")
+  })
+  log.Fatal(http.ListenAndServe(":8000", nil))
+}
