bbaf1b4d8938ca3ad047a8f870fe27518daeb7e7
Votre nom ou pseudo <Votre@email.comEliPamp>
2017-04-05 15:47:52 +0200

    rajout de l'incrementation

--- main.go
+++ main.go
@@ -3,14 +3,28 @@ import (
   "log"
   "net/http"
   "fmt"
-  "html"
+  "strconv"
+  "sync"
 
 )
 
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+  mutex.Lock()
+  counter++
+  fmt.Fprintf(w, strconv.Itoa(counter))
+  mutex.Unlock()
+}
+
 func main () {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
+  http.HandleFunc("/", echoString)
+  http.HandleFunc("/increment", incrementCounter)
 
   http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Hi")
